ec32f510bf659e480485bdd8c1e1558891cee642
b-olle <baltasarolle@gmail.com>
2026-03-09 21:10:52 -0300

    Agrego versión definitiva del src de yolo_node

---
+++ deteccion_objetos/ros2_ws/src/yolo_node/yolo_node/yolo_detect_wrapper.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import os
+import sys
+
+# Forzar el venv. NO eliminar (verificado el 20/12/25)
+sys.path.insert(0, '/home/baltasar/proyecto final/my_env/lib/python3.12/site-packages')
+
+from yolo_node.yolo_detect_node import main
+# from yolo_node.yolo_detect_node_OK_1 import main
+
+if __name__ == '__main__':
+    main()
+
